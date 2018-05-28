002164a7ebb6c60ad802e9cd04cadb044e8b3096
Mark Huntjens <mark.huntjens@isatis-business-solutions.nl>
2018-05-28 09:00:14 +0200

    Just starting

---
+++ Chapter 7 - build a house/Location.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_7___build_a_house
+{
+    abstract class Location
+    {
+        public Location(string name)
+        {
+            Name = name;
+        }
+        public Location[] Exits;
+        public string Name { get; private set; }
+
+        public virtual string Description
+        {
+            get
+            {
+                string description = $"You're standing in the {Name}." +
+                                     $"You see exits to the following places: ";
+                for (int i = 0;i<Exits.Length;i++)
+                {
+                    description += $" {Exits[i]}";
+                    if (i != Exits.Length - 1)
+                        description += ",";
+                }
+
+                description += ".";
+                return description;
+            }
+        }
+    }
+}
